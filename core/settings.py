126c46c78471e80b15661a6d2a70a91c7ad87e5f
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-07-20 19:38:03 +0500

    awdawd

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILED_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
