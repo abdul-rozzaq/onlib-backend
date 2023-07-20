e12e0ba4c71f9a10cd40ff69478d6ebc45eaf4e4
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-07-20 20:00:31 +0500

    awdad

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -10,3 +13,8 @@ urlpatterns = [
     
     path('api-registration/', include('dj_rest_auth.registration.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
