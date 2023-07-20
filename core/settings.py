46fbe3ef2b6c8d9f37dedc892c0c727c7a17e525
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-07-20 19:56:57 +0500

    awdad

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ else:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+ 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
